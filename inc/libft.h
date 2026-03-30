f30b44c0731381c467bc66c7df29fb9912af4f97
bassaaaa <white.galaxys2.album2@gmail.com>
2026-03-30 16:15:57 +0900

    プロトタイプを追加

--- inc/libft.h
+++ inc/libft.h
@@ -6,7 +6,7 @@
 /*   By: tsito <tsito@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 20:48:57 by tsito             #+#    #+#             */
-/*   Updated: 2026/03/27 20:52:00 by tsito            ###   ########.fr       */
+/*   Updated: 2026/03/30 16:15:27 by tsito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,11 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+
+int	ft_isalpha(char c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
 
 #endif
