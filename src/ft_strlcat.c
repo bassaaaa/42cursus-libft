37644f5e175bfbcc91dc71f8e10070077f7d9b17
bassaaaa <white.galaxys2.album2@gmail.com>
2026-03-31 21:13:16 +0900

    ft_strlcatを追加

---
+++ src/ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsito <tsito@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/31 20:28:27 by tsito             #+#    #+#             */
+/*   Updated: 2026/03/31 21:12:22 by tsito            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+{
+	size_t	d_len;
+	size_t	s_len;
+	size_t	i;
+
+	d_len = ft_strlen(dst);
+	s_len = ft_strlen(src);
+	if (dstsize <= d_len)
+		return (d_len + s_len);
+	i = 0;
+	while (src[i] && i < dstsize - d_len - 1)
+	{
+		dst[d_len + i] = src[i];
+		i++;
+	}
+	dst[d_len + i] = '\0';
+	return (d_len + s_len);
+}
