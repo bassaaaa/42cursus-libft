ad5aa0efb63ab88039fca1cecd4ce3c463d4aae9
bassaaaa <white.galaxys2.album2@gmail.com>
2026-04-02 23:09:23 +0900

    プロトタイプを追加

--- inc/libft.h
+++ inc/libft.h
@@ -6,7 +6,7 @@
 /*   By: tsito <tsito@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 19:33:09 by tsito             #+#    #+#             */
-/*   Updated: 2026/04/01 19:33:11 by tsito            ###   ########.fr       */
+/*   Updated: 2026/04/02 22:50:19 by tsito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,17 @@ int		ft_tolower(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *restrict dst, const char *restrict src,
+			size_t dstsize);
+size_t	ft_strlcat(char *restrict dst, const char *restrict src,
+			size_t dstsize);
+char	*ft_strchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_atoi(const char *str);
 
 #endif
