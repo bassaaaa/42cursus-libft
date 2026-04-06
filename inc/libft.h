8fbbafc26d3c35196c17b1e671ffc9518f65ccd1
bassaaaa <white.galaxys2.album2@gmail.com>
2026-04-06 19:48:54 +0900

    ft_itoaを追加

--- inc/libft.h
+++ inc/libft.h
@@ -6,7 +6,7 @@
 /*   By: tsito <tsito@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 19:33:09 by tsito             #+#    #+#             */
-/*   Updated: 2026/04/03 22:01:24 by tsito            ###   ########.fr       */
+/*   Updated: 2026/04/06 18:48:33 by tsito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
