c67a5ef4e95c826ee52be737cafe9e9cd6f32260
bassaaaa <white.galaxys2.album2@gmail.com>
2026-04-10 18:28:30 +0900

    ft_striteriを追加

--- inc/libft.h
+++ inc/libft.h
@@ -6,7 +6,7 @@
 /*   By: tsito <tsito@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 19:33:09 by tsito             #+#    #+#             */
-/*   Updated: 2026/04/10 18:11:32 by tsito            ###   ########.fr       */
+/*   Updated: 2026/04/10 18:15:11 by tsito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 // convert
 int		ft_atoi(const char *str);
 char	*ft_itoa(int n);
