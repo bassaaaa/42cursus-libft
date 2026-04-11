04550064fa53d196d636b41590585fbfbc7bb77a
bassaaaa <white.galaxys2.album2@gmail.com>
2026-04-11 15:15:19 +0900

    ft_lstlastを追加

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tsito <tsito@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 19:33:09 by tsito             #+#    #+#             */
-/*   Updated: 2026/04/11 14:49:19 by tsito            ###   ########.fr       */
+/*   Updated: 2026/04/11 15:04:26 by tsito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,5 +72,6 @@ typedef struct s_list
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new_elem);
 int					ft_lstsize(t_list *lst);
+t_list				*ft_lstlast(t_list *lst);
 
 #endif
