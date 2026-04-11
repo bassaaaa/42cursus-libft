f235f69230b4f655217c8ce863dc1c6ba3ad7976
bassaaaa <white.galaxys2.album2@gmail.com>
2026-04-11 15:03:36 +0900

    ft_lstsizeを追加

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tsito <tsito@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 19:33:09 by tsito             #+#    #+#             */
-/*   Updated: 2026/04/11 14:44:18 by tsito            ###   ########.fr       */
+/*   Updated: 2026/04/11 14:49:19 by tsito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,5 +71,6 @@ typedef struct s_list
 
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new_elem);
+int					ft_lstsize(t_list *lst);
 
 #endif
