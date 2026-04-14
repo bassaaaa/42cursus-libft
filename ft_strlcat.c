f010147129424307e1467f9a733db5d8e93500c0
bassaaaa <white.galaxys2.album2@gmail.com>
2026-04-14 21:51:30 +0900

    戻り値を修正

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tsito <tsito@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 19:28:57 by tsito             #+#    #+#             */
-/*   Updated: 2026/04/01 19:28:58 by tsito            ###   ########.fr       */
+/*   Updated: 2026/04/14 21:39:23 by tsito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	d_len = ft_strlen(dst);
 	s_len = ft_strlen(src);
 	if (dstsize <= d_len)
-		return (d_len + s_len);
+		return (dstsize + s_len);
 	i = 0;
 	while (src[i] && i < dstsize - d_len - 1)
 	{
