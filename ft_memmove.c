fa115382f7aa194b8028b586132dde907a54925f
bassaaaa <white.galaxys2.album2@gmail.com>
2026-04-15 23:47:48 +0900

    キャストを追加

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: tsito <tsito@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 19:30:07 by tsito             #+#    #+#             */
-/*   Updated: 2026/04/14 21:46:03 by tsito            ###   ########.fr       */
+/*   Updated: 2026/04/15 23:44:12 by tsito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	unsigned char		*d_ptr;
 	const unsigned char	*s_ptr;
 
-	d_ptr = dst;
-	s_ptr = src;
+	d_ptr = (unsigned char *)dst;
+	s_ptr = (unsigned char *)src;
 	if (s_ptr < d_ptr && d_ptr < s_ptr + len)
 	{
 		d_ptr += len;
