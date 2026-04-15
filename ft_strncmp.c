19ef65a94d19cc865c20135080476de1c4381549
bassaaaa <white.galaxys2.album2@gmail.com>
2026-04-15 15:47:42 +0900

    ループ条件を修正

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tsito <tsito@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 19:26:01 by tsito             #+#    #+#             */
-/*   Updated: 2026/04/01 19:26:06 by tsito            ###   ########.fr       */
+/*   Updated: 2026/04/15 15:40:05 by tsito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	size_t	i;
+
 	if (n == 0)
 		return (0);
-	while (n > 0 && *s1 && (unsigned char)*s1 == (unsigned char)*s2)
-	{
-		s1++;
-		s2++;
-		n--;
-	}
-	return ((unsigned char)*s1 - (unsigned char)*s2);
+	i = 0;
+	while (i < n - 1 && s1[i] && (unsigned char)s1[i] == (unsigned char)s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
