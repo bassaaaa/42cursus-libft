acbf80e120b5f995d9d768970bed6d584d9c54f4
bassaaaa <white.galaxys2.album2@gmail.com>
2026-04-15 15:49:36 +0900

    条件の順序を修正

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: tsito <tsito@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 21:21:30 by tsito             #+#    #+#             */
-/*   Updated: 2026/04/02 22:56:05 by tsito            ###   ########.fr       */
+/*   Updated: 2026/04/15 15:48:57 by tsito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	if (!*needle)
 		return ((char *)haystack);
 	n_len = ft_strlen(needle);
-	while (*haystack && n_len <= len)
+	while (n_len <= len && *haystack)
 	{
 		if (ft_strncmp(haystack, needle, n_len) == 0)
 			return ((char *)haystack);
