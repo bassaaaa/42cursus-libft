439818f441441c9f10de5a2e1155db776769d254
bassaaaa <white.galaxys2.album2@gmail.com>
2026-04-17 14:05:03 +0900

    末尾のnextをNULLにしていたのを削除

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: tsito <tsito@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 15:16:32 by tsito             #+#    #+#             */
-/*   Updated: 2026/04/11 15:27:33 by tsito            ###   ########.fr       */
+/*   Updated: 2026/04/17 14:03:30 by tsito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,4 @@ void	ft_lstadd_back(t_list **lst, t_list *new_elem)
 		*lst = new_elem;
 	else
 		last->next = new_elem;
-	new_elem->next = NULL;
 }
